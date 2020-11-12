97b0b1bcb97e822a3cd0b64bb5658dfce2a8a174
Aidil Qayyum <aidilq22@gmail.com>
2020-11-13 00:43:22 +0800

    initial commit

---
+++ we_deliver_admin/lib/core/food.dart
@@ -0,0 +1,20 @@
+class Food {
+  String fid,
+         fname,
+         fprice,
+         fdesc,
+         fimage,
+         ftime;
+
+  Food (
+    {
+      this.fid,
+      this.fname,
+      this.fprice,
+      this.fdesc,
+      this.fimage,
+      this.ftime
+    }
+  );
+
+}
